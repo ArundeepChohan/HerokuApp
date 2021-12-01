d8e571219d634a4b7eba8c0199181d05bccd9a59
Arundeep Chohan <arundeepchohan2009@hotmail.com>
2021-11-30 22:51:16 -0800

    Added port

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-2$-05$^hph59mje#k#r7coxzj!*1l_dreh+p(*1u#0i0@(-a3l'
+SECRET_KEY = 'django-insecure-2$-05$^hph59mje#k#r7coxzj!*1l_dreh+p(*1u#0i0@(-a3l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
