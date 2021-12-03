7406b498087cd9db049ee082121f66d1986c50a8
Arundeep Chohan <arundeepchohan2009@hotmail.com>
2021-12-02 17:03:16 -0800

    PORT fixes

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -26,9 +26,11 @@ import os
 SECRET_KEY = os.getenv("SECRET_KEY", default="dev key")
 PORT = os.getenv("PORT", default=5000)
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
 
-ALLOWED_HOSTS = ["https://arundeepchohan.herokuapp.com/"]
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+
+ALLOWED_HOSTS = ["arundeepchohan.herokuapp.com/","127.0.0.1:5000/"]
 
 
 # Application definition
