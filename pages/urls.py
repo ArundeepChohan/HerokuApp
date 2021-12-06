887e3a41f8f0cbd0a4b569c2bd4d380e2b4a452e
Arundeep Chohan <arundeepchohan2009@hotmail.com>
2021-12-05 16:44:12 -0800

    Adding homepage

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import homePageView
+from . import views
 
 urlpatterns = [
-    path("", homePageView, name="home"),
+    path("", views.homePageView, name="home"),
 ]
