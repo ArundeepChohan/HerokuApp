434ca3a14e2ec4a69b4bb6e5a119b435d3f0d01a
Arundeep Chohan <arundeepchohan2009@hotmail.com>
2021-12-09 15:44:22 -0800

    Adding homepage

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -18,6 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("pages.urls")),  # new
-
+    path("", include("pages.urls")),  
 ]
