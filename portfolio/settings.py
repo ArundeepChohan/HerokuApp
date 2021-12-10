5d3c25bb3b297370b6542b2d7d0aa8cd073f41b3
Arundeep Chohan <arundeepchohan2009@hotmail.com>
2021-12-10 00:39:31 -0800

    Adding Login styles

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -132,5 +132,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 import django_heroku
 django_heroku.settings(locals())
 
-LOGIN_REDIRECT_URL = '/login'
+LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/' 
