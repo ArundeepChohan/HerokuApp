f046fd8938d96c365e49917a0157df2180592e0f
Arundeep Chohan <arundeepchohan2009@hotmail.com>
2021-12-09 23:15:26 -0800

    Adding Login styles

--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required. 
 
+@login_required
 def index(request):
-    return render(request,'base.html')
+    # now the index function renders the home page
+    return render(request,'home.html') 
