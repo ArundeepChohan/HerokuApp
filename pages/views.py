fdeea7c7ee3a1e47e2bb4072e935c725a5a8911f
Arundeep Chohan <arundeepchohan2009@hotmail.com>
2021-12-09 22:48:59 -0800

    Adding Login styles

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,4 @@
-##from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    ##return HttpResponse("Hello, world. You're at the polls index.")
     return render(request,'registration/login.html')
