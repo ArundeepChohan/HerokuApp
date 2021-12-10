ce6b2b5252a9f98881385f1b4c902791c9349348
Arundeep Chohan <arundeepchohan2009@hotmail.com>
2021-12-09 22:59:21 -0800

    Adding Login styles

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-
-from . import views
+from django.views.generic.base import TemplateView # new
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', TemplateView.as_view(template_name='home.html'), name='home')
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request,'registration/login.html')
+    return render(request,'base.html')
