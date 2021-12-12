a95f89c04cbc99941b2d89b2592f32e31188625d
Arundeep Chohan <arundeepchohan2009@hotmail.com>
2021-12-12 04:03:10 -0800

    Adding session logout

--- pages/urls.py
+++ pages/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from django.views.generic.base import TemplateView # new
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='home.html'), name='home')
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -137,7 +137,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 import django_heroku
 django_heroku.settings(locals())
 
-LOGIN_URL = 'home'
+LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 
