d2f6196230f81971133007afd7721915bb152f27
Arundeep Chohan <arundeepchohan2009@hotmail.com>
2021-12-12 03:44:57 -0800

    Adding session logout

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def index(request):
     # now the index function renders the home page
-    return render(request,'home.html') 
+    return render(request,'home.html')
