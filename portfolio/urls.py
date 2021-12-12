60deb7d73415dce41750bbad4a1e1f8680a1e9d9
Arundeep Chohan <arundeepchohan2009@hotmail.com>
2021-12-12 12:13:44 -0800

    Adding session logout

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -17,8 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path(r'^admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('', include('pages.urls')),
-    path(r'^accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
       
 ]
