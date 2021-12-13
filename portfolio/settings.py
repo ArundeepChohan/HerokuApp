80ad711bde9b2394edd9b5f98fc19354765c60ab
Arundeep Chohan <arundeepchohan2009@hotmail.com>
2021-12-13 00:02:30 -0800

    Adding session logout

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -144,10 +144,10 @@ SESSION_SERIALIZER = 'django.contrib.sessions.serializers.PickleSerializer'
 SESSION_ENGINE = (
 'django.contrib.sessions.backends.cache'
 )
-""" SESSION_COOKIE_AGE = 10  # change expired session
+SESSION_COOKIE_AGE = 10  # change expired session
 SESSION_IDLE_TIMEOUT = 10  # logout
 
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 SESSION_SAVE_EVERY_REQUEST = True
-SESSION_TIMEOUT_REDIRECT = '/accounts/logout/' """
+SESSION_TIMEOUT_REDIRECT = '/accounts/logout/' 
 
