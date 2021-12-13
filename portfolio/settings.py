66a140b4f62aa434d3987911760a59b9567d1f79
Arundeep Chohan <arundeepchohan2009@hotmail.com>
2021-12-13 00:21:39 -0800

    Adding session logout

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'pages.middleware.AutoLogout'
+    'portfolio.middleware.AutoLogout'
 ]
 
 ROOT_URLCONF = 'portfolio.urls'
