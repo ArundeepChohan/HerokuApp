9f6da8c3af4a7583729ca1b19ead0dfdc681bc0c
Arundeep Chohan <arundeepchohan2009@hotmail.com>
2021-12-14 14:51:30 -0800

    Side bar

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -142,7 +142,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'pages/static'),
 ]
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
