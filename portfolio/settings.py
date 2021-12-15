751d1c12b53f3d508cd0965983f66d40ead8245e
Arundeep Chohan <arundeepchohan2009@hotmail.com>
2021-12-14 17:26:21 -0800

    Side bar

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -158,10 +158,10 @@ SESSION_SERIALIZER = 'django.contrib.sessions.serializers.PickleSerializer'
 SESSION_ENGINE = (
 'django.contrib.sessions.backends.cache'
 )
-SESSION_COOKIE_AGE = 10  # change expired session
+""" SESSION_COOKIE_AGE = 10  # change expired session
 
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
-SESSION_SAVE_EVERY_REQUEST = True 
+SESSION_SAVE_EVERY_REQUEST = True  """
 
 import django_heroku
 django_heroku.settings(locals(), staticfiles=False)
