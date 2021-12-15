202b679a45c6b9d15aeca6310389475e30f08014
Arundeep Chohan <arundeepchohan2009@hotmail.com>
2021-12-15 02:00:27 -0800

    Custom admin user

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+class UserAdminCustom(BaseUserAdmin):
+   list_display = ('email', 'phone', 'profile-pic','date-created', 'is_staff', 'is_superuser')
+   list_filter = ('is_staff', 'is_superuser')
+   search_fields = ('username', )
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdminCustom)
