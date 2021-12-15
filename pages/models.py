7e5db7ca2a36093bafa5544bc59ff89b109aabb7
Arundeep Chohan <arundeepchohan2009@hotmail.com>
2021-12-15 04:05:24 -0800

    Custom admin user

--- pages/models.py
+++ pages/models.py
@@ -4,7 +4,6 @@ from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
-    #is_staff = models.BooleanField(default=False)
     #is_active = models.BooleanField(default=True)
     date_created = models.DateTimeField(default=timezone.now)
     #profile_pic = models.
