a45104c4cda9b1fe612953f366c8c2b807041e7b
Arundeep Chohan <arundeepchohan2009@hotmail.com>
2021-12-15 00:48:31 -0800

    Side bar

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -146,10 +146,7 @@ if DEBUG=='False':
 
     STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
     DEFAULT_FILE_STORAGE = 'hello_django.storage_backends.PublicMediaStorage'
-else:
-    STATIC_URL = '/staticfiles/'
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
