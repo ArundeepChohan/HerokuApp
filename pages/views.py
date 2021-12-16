4b7ecfbc5876126fe6cc8adb71bed4ff8011c57a
Arundeep Chohan <arundeepchohan2009@hotmail.com>
2021-12-16 09:46:37 -0800

    Static and Media Upload to AWS S3

--- pages/views.py
+++ pages/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
 
-from .models import Document
+from .models import Document, PrivateDocument
 
 
 class DocumentCreateView(CreateView):
@@ -16,6 +17,18 @@ class DocumentCreateView(CreateView):
         documents = Document.objects.all()
         context['documents'] = documents
         return context
-        
+
+@method_decorator(login_required, name='dispatch')
+class PrivateDocumentCreateView(CreateView):
+    model = PrivateDocument
+    fields = ['upload', ]
+    success_url = reverse_lazy('profile')
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        return super().form_valid(form)  
+             
 def index(request):
     return render(request,'home.html')
