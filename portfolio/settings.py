b4b96659ef774c29fff9cdba4c19e67a5c2e9183
Arundeep Chohan <arundeepchohan2009@hotmail.com>
2021-12-16 00:49:38 -0800

    Static and Media Upload to AWS S3

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -162,8 +162,6 @@ STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 ##STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 ##DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    
-
 
 
 #Todo: Add settings for not prod
