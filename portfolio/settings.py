c2d9eba940a33476714293c34394a2824031dc12
Arundeep Chohan <arundeepchohan2009@hotmail.com>
2021-12-16 03:16:20 -0800

    Static and Media Upload to AWS S3

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -149,7 +149,7 @@ AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
-AWS_LOCATION = 'static'
+
 AWS_S3_SIGNATURE_VERSION = 's3v4'
 AWS_S3_REGION_NAME = 'us-east-2'
 AWS_DEFAULT_ACL = None
@@ -157,10 +157,11 @@ AWS_DEFAULT_ACL = None
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'pages/static'),
 ]
+AWS_STATIC_LOCATION = 'static'
+STATICFILES_STORAGE = 'portfolio.storage_backends.StaticStorage'
+STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_STATIC_LOCATION)
 
-STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 ##STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 AWS_PUBLIC_MEDIA_LOCATION = 'media/public'
 DEFAULT_FILE_STORAGE = 'portfolio.storage_backends.PublicMediaStorage'
