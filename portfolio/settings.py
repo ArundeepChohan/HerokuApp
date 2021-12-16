75cc9e232180db639bd63cad6f34401bb9cf0d42
Arundeep Chohan <arundeepchohan2009@hotmail.com>
2021-12-16 00:14:44 -0800

    Static and Media Upload to AWS S3

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -157,7 +157,8 @@ if DEBUG=='False':
     STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    ##STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'pages/static'),
