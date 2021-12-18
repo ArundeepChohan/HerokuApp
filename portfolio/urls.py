f4cafe5d3f20f982bed8eded560589124ac95805
Arundeep Chohan <arundeepchohan2009@hotmail.com>
2021-12-17 20:18:10 -0800

    Static and Media Upload to AWS S3

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -19,7 +19,7 @@ from django.conf.urls.static import static
 from django.urls import path, include
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('', include('pages.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
       
