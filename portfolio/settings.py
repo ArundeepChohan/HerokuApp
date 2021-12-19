338eb135b87bdb1d685a7f1bb7a6f39c0a6949d2
Arundeep Chohan <arundeepchohan2009@hotmail.com>
2021-12-19 03:05:14 -0800

    Static and Media Upload to AWS S3

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -165,6 +165,7 @@ STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_STATIC_LOCATION)
 STATICFILES_STORAGE = 'portfolio.storage_backends.StaticStorage'
 
 MEDIAFILES_LOCATION = 'media'
+MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION )
 DEFAULT_FILE_STORAGE = 'portfolio.storage_backends.MediaStorage'
 
 """ else:
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
       
 ]
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
